34f9f71bf123d36b2aab2de1b970e30ae7a30588
moran-_d <moran-_d@epitech.eu>
2015-03-20 22:33:38 +0100

    modif .h +add

--- etape_1/src/elcrypt.c
+++ etape_1/src/elcrypt.c
@@ -5,7 +5,7 @@
 ** Login   <moran-_d@epitech.net>
 ** 
 ** Started on  Fri Mar 20 22:06:10 2015 moran-_d
-** Last update Fri Mar 20 22:15:54 2015 moran-_d
+** Last update Fri Mar 20 22:21:20 2015 moran-_d
 */
 
 #include <string.h>
@@ -17,12 +17,20 @@ int elcrypt(elc *elc, int turn)
   char buf[BBLOCK];
   int64_t block;
   int len;
-  
+
+  memset(buf, 0, BBLOCK);
   while ((len = read(elc->fin, buf, BBLOCK)) == BBLOCK)
     {
       block = (int64_t)buf;
       block = feistel(elc, block, elc->mode);
-      memcpy(buf, block, BBLOCK);
-      //      write(elc->fout, 
+      memcpy(buf, &block, BBLOCK);
+      write(elc->fout, buf, BBLOCK);
+      memset(buf, 0, BBLOCK);
     }
+  if (len < 0)
+    return (printf("Error during reading\n"));
+  block = add_padding(buf, len);
+  memcpy(buf, &block, BBLOCK);
+  write(elc->fout, buf, BBLOCK);  
+  return (0);
 }
---
+++ etape_1/src/feistel.c
@@ -0,0 +1,12 @@
+/*
+** feistel.c for feistel in /home/moran-_d/rendu/elcrypt/etape_1/src
+** 
+** Made by moran-_d
+** Login   <moran-_d@epitech.net>
+** 
+** Started on  Fri Mar 20 22:22:03 2015 moran-_d
+** Last update Fri Mar 20 22:29:41 2015 moran-_d
+*/
+
+#include "elcrypt.h"
+
