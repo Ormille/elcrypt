7ec29d126d576c0d2a833568880390e2eb258080
terran_j <terran_j@epitech.eu>
2015-03-20 21:07:51 +0100

    nouvel agencement

---
+++ etape_1/include/elcrypt.h
@@ -0,0 +1,16 @@
+/*
+** elcrypt.h for elcrypt in /home/terran_j/rendu/elcrypt/etape_1
+**
+** Made by Julie Terranova
+** Login   <terran_j@epitech.net>
+**
+** Started on  Fri Mar 20 21:06:36 2015 Julie Terranova
+** Last update Fri Mar 20 21:06:52 2015 Julie Terranova
+*/
+
+#ifndef ELCRYPT_H_
+# define ELCRYPT_H_
+
+
+
+#endif
---
+++ etape_1/src/main.c
@@ -0,0 +1,17 @@
+/*
+** main.c for elcrypt in /home/terran_j/rendu/elcrypt/etape_1
+**
+** Made by Julie Terranova
+** Login   <terran_j@epitech.net>
+**
+** Started on  Fri Mar 20 21:05:57 2015 Julie Terranova
+** Last update Fri Mar 20 21:06:20 2015 Julie Terranova
+*/
+
+#include "elcrypt.h"
+
+int	main()
+{
+
+  return (0);
+}
