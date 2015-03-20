99b272e99040f04ec76d8f5fe3a4f1648ca12c85
terran_j <terran_j@epitech.eu>
2015-03-20 22:15:24 +0100

    verif args

--- etape_1/src/main.c
+++ etape_1/src/main.c
@@ -5,13 +5,77 @@
 ** Login   <terran_j@epitech.net>
 **
 ** Started on  Fri Mar 20 21:05:57 2015 Julie Terranova
-** Last update Fri Mar 20 21:06:20 2015 Julie Terranova
+** Last update Fri Mar 20 22:15:09 2015 Julie Terranova
 */
 
+#include <stdio.h>
 #include "elcrypt.h"
 
-int	main()
+int	crypt(char **av, elc opt)
 {
+  int mask;
+  int i;
 
+  i = (mask = 0);
+  while (av[i] && i != 9)
+    {
+      if ((mask & 1) == 0 && strcmp("-f", av[i]) == 0)
+	{
+	  if ((opt.fin = open(av[i + 1], O_RDONLY)) == -1)
+	    return (printf("Error with source file\n"));
+	  mask = mask & 1;
+	}
+      else if ((mask & 1) == 1)
+	return (printf("-f already declared\n"));
+      if ((mask & 2) == 0 && strcmp("-o", av[i]) == 0)
+	{
+	  if ((opt.fout = open(av[i + 1], O_CREAT | O_EXCL | O_RDWR, 0644))
+	      == -1)
+	    return (printf("Error with destination file\n"));
+	  mask = mask & 2;
+	}
+      else if ((mask & 2) == 1)
+	return (printf("-o already declared\n"));
+      if ((mask & 4) == 0 && strcmp("-k", av[i]) == 0)
+	{
+	  opt.key = strtoll(av[i + 1], NULL, 10);
+	  mask = mask & 4;
+	}
+      else if ((mask & 4) == 1)
+	return (printf("-k already declared\n"));
+      i++;
+    }
+  return (0);
+}
+
+void	verify_arguments(char **av)
+{
+  int i;
+  elc opt;
+
+  i = 0;
+  while (av[i])
+    {
+      if (strcmp("-d", av[i]) == 0)
+	{
+	  opt.mode = DECRYPT;
+	  crypt(av, opt);
+	  break;
+	}
+      if (strcmp("-e", av[i]) == 0)
+	{
+	  opt.mode = ENCRYPT;
+	  crypt(av, opt);
+	  break;
+	}
+      a++;
+    }
+}
+
+int	main(int ac, char **av)
+{
+  if (ac == 9)
+    verify_arguments(av);
+  printf("Usage: ./elcrypt [-d | -e] [-f src_file] [-o dest_file] [-k key]\n");
   return (0);
 }
